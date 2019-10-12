25ecea328ba901b1165c3b92125a7b7dcfc88fed
unknown <vemareddy119@gmail.com>
2019-10-12 15:22:40 +0530

    fdsfs

--- JavaFirstApp/src/com/java/Test.java
+++ JavaFirstApp/src/com/java/Test.java
@@ -5,6 +5,9 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Vema reddy");
+		for(int i=0;i<=0;i++) {
+			System.out.println(i);
+		}
 	}
 
 }
